25fc88a7375a7ce74f7a62851de7642d47d6fd21
darknight1050 <darkdeveloping1050@gmail.com>
2021-10-24 19:17:53 +0200

    Fix Allocation of Adafruit_NeoPixel

--- NeoPixelDisplay.hpp
+++ NeoPixelDisplay.hpp
@@ -6,9 +6,7 @@ class NeoPixelDisplay : public Display {
 private:
     Adafruit_NeoPixel neoPixelAPI;
 public:
-    NeoPixelDisplay(uint16_t pixelCount, int16_t pin, neoPixelType type = NEO_GRB + NEO_KHZ800) : Display(pixelCount) {
-        neoPixelAPI = Adafruit_NeoPixel(pixelCount, pin, type);
-    }
+    NeoPixelDisplay(uint16_t pixelCount, int16_t pin, neoPixelType type = NEO_GRB + NEO_KHZ800) : Display(pixelCount), neoPixelAPI(Adafruit_NeoPixel(pixelCount, pin, type)) { }
     
     void begin() {
         neoPixelAPI.begin();
